c9832378e34ab1f8a960ddcad04efe4d1ec51eaf
SokuGreyrat <moiseslopezchavez8@gmail.com>
2026-09-08 22:53:02 -0600

    feat: front game 1 version 1

--- animoon/app/src/main/java/com/example/animoon/ui/games/Game1Activity.kt
+++ animoon/app/src/main/java/com/example/animoon/ui/games/Game1Activity.kt
@@ -1,4 +1,13 @@
 package com.example.animoon.ui.games
 
-class Game1Activity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.animoon.R
+
+class Game1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game1)
+    }
 }
